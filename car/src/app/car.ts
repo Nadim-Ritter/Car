9e5abd7f7aaa6b274d204c698ec95fd81b74f620
Nadim-Ritter <nadim.ritter@ch.ibm.com>
2019-06-20 14:05:16 +0200

    add car class

---
+++ car/src/app/car.ts
@@ -0,0 +1,11 @@
+export class Car {
+
+    public Id: number;
+    public Power: number;
+    public VMax: number;
+    public Color: string;
+    public ReleaseYear: Date;
+    public AmountSeats: number;
+    public Brand: string;
+    public Model: string;
+}
