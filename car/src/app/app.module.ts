f2a4d11e418ca77463ebacfae8e78f0a14f3c159
severin <demchukseverin@gmail.com>
2019-06-20 15:47:04 +0200

    Add angular material

--- car/src/app/app.module.ts
+++ car/src/app/app.module.ts
@@ -6,19 +6,28 @@ import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { FormComponent } from './form/form.component';
 import { RowComponent } from './row/row.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatInputModule} from '@angular/material';
+import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
     FormComponent,
-    RowComponent,
-    MatFormFieldModule
+    RowComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
